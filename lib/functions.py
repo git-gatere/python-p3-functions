93ae4942178993fc7f36a1b42688c66151a21a53
gatere <mercynjerigatere@gmail.com>
2024-05-22 09:45:42 +0300

    Adds lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,15 @@
-#!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer() :
+    print("Hello programmer")
 
-def greet(name):
-    pass
+def greet(name) :
+    print(f"Hello, {name}")
 
-def greet_with_default(name="programmer"):
-    pass
+def greet_with_default (name="programmer") :
+    print(f"Hello, {name}")
 
-def add(num1, num2):
-    pass
+def add (num1, num2) :
+    return num1+num2
 
-def halve(number):
-    pass
+def halve (number) :
+    return number/2
